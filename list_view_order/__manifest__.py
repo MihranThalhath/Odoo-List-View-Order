bfe5d353bf83c5471aa0e169dc4368e317f2c604
Mihran Thalhath <mihranz7@gmail.com>
2025-11-18 22:27:57 +0530

    fix: fetch field component correctly
    earlier if the field component was empty if the field
    didn't have a widget. use field type fi widget is missing
    
    fixes #1

--- list_view_order/__manifest__.py
+++ list_view_order/__manifest__.py
@@ -5,7 +5,7 @@
     "website": "https://www.mihranthalhath.com",
     "license": "OPL-1",
     "category": "Tools",
-    "version": "19.0.1.0.1",
+    "version": "19.0.1.0.2",
     "depends": ["web"],
     "data": [
         "security/res_group.xml",
